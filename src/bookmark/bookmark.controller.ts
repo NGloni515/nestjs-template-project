fb693541655acac27298868dc9e1fac93652e573
Angeloni Cuesta <angelonicuesta@Angelonis-MacBook-Pro.local>
2022-11-10 03:04:17 -0300

    bookmark controller ready

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { BookmarkService } from './bookmark.service';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('bookmark')
-export class BookmarkController {}
+export class BookmarkController {
+    constructor(private bookmarkService: BookmarkService) { }
+
+    @Get()
+    getBookmarks(@GetUser('id') userId: number) {
+        return this.bookmarkService.getBookmarks(
+            userId,
+        )
+    }
+
+    @Get(':id')
+    getBookmarkById(@GetUser() userId: number, @Param('id', ParseIntPipe) bookmarkId: number) {
+        return this.bookmarkService.getBookmarkById(
+            userId,
+            bookmarkId,
+        )
+    }
+
+    @Post()
+    createBookmark(@GetUser() userId: number, @Body() dto: CreateBookmarkDto) {
+        return this.bookmarkService.createBookmark(
+            userId,
+            dto,
+        )
+    }
+
+    @Patch(':id')
+    editBookmarkById(@GetUser() userId: number, @Param('id', ParseIntPipe) bookmarkId: number, @Body() dto: EditBookmarkDto) {
+        return this.bookmarkService.editBookmarkById(
+            userId,
+            bookmarkId,
+            dto,
+        )
+    }
+
+    @Delete(':id')
+    deleteBookmarkById(@GetUser() userId: number, @Param('id', ParseIntPipe) bookmarkId: number) {
+        return this.bookmarkService.deleteBookmarkById(
+            userId,
+            bookmarkId,
+        )
+    }
+}
